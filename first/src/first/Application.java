465dd713b7cebcd16a376cd361cd18bbbce6ad99
Trey <treylab004@gmail.com>
2020-06-04 20:52:40 +0100

    Signed-off-by: Trey <treylab004@gmail.com>

---
+++ first/src/first/Application.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		// standard main method, gets program up and running
+		
+	System.out.println ("GangNam");
+		
+		
+	}
+}
